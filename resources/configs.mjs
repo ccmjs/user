7dbfd80bd5061db0ab4620c0ffe203b7542d4edb
akless <andre.kless@web.de>
2026-09-17 23:36:43 +0200

    Comment out Google authentication provider configuration in demo settings for clarity and future reference.

--- resources/configs.mjs
+++ resources/configs.mjs
@@ -1,11 +1,19 @@
 /** Demo with local registration and an independent Google authentication component. */
 export const demo = {
   registration: true,
-  providers: [["ccm.instance", "../google_login/ccm.google_login.mjs", {
-    url: "https://ccmjs.github.io/google_login/auth.html",
-    server: "http://localhost:8080",
-    realm: "ccm",
-    displayName: "name", // name, given_name, family_name, email or id
-    picture: true,
-  }]],
+  /*
+  providers: [
+    [
+      "ccm.instance",
+      "https://ccmjs.github.io/google_login/ccm.google_login.mjs",
+      {
+        url: "https://ccmjs.github.io/google_login/auth.html",
+        server: "http://localhost:8080",
+        realm: "ccm",
+        displayName: "name", // name, given_name, family_name, email or id
+        picture: true,
+      },
+    ],
+  ],
+   */
 };
